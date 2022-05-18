ec32382c744be1aef9b6fed1122f63d7324a30e5
Matthijs Groen <matthijs.groen@gmail.com>
2022-05-18 14:06:43 +0200

    update

--- src/matthijs.ts
+++ src/matthijs.ts
@@ -141,13 +141,6 @@ export const createRoot = (root: HTMLElement): Renderer => {
   let previousRenderTree: VElement;
   let renderJsx: Producer;
 
-  // root.addEventListener(
-  //   "keypress",
-  //   (e) => {
-  //     e.preventDefault();
-  //   },
-  // );
-
   Array.from(root.childNodes).forEach((c) => root.removeChild(c));
   const render = (contents: Producer) => {
     callIndex = 0;
@@ -156,10 +149,9 @@ export const createRoot = (root: HTMLElement): Renderer => {
     const result = contents();
 
     // Update our 'global' initial render, so that the refs are kept.
-
-    // reconciliation
     initialRender = false;
 
+    // reconciliation
     patch(
       root,
       element("root", {}, [result]),
