cc6da814a13624d68387a6ffbb960ffca06ee81d
Matthijs Groen <matthijs.groen@gmail.com>
2022-05-18 14:59:49 +0200

    remove app

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,10 @@ const root = createRoot(document.getElementById("root"));
 
 const MyText: Component = ({ children }) => <p class="Foo">{children}</p>;
 
-export const App: Component = () => {
-  return (
-    <div>
-      <MyText>
-        Hello <strong>world!</strong>
-      </MyText>
-    </div>
-  );
-};
-
-root.render(<App />);
+root.render(
+  <div>
+    <MyText>
+      Hello <strong>world!</strong>
+    </MyText>
+  </div>
+);
