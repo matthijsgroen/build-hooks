876c3af37deefeaf959ea045465710c17203a1e5
Matthijs Groen <matthijs.groen@gmail.com>
2022-05-25 08:44:35 +0200

    remove subscription part

--- redux/redux.js
+++ redux/redux.js
@@ -1,20 +1,12 @@
 const createStore = (reducer, initialState) => {
   let state = reducer(initialState, { type: "__INIT" });
-  let subscribers = [];
 
   return {
-    subscribe: (f) => {
-      subscribers = subscribers.concat(f);
-      return () => {
-        subscribers = subscribers.filter((subscriber) => subscriber !== f);
-      };
-    },
     getState: () => state,
     dispatch: (action) => {
       const newState = reducer(state, action);
       if (state !== newState) {
         state = newState;
-        subscribers.forEach((f) => f());
       }
     },
   };
